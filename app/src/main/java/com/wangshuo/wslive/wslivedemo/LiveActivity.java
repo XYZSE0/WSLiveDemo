e206a536bf1ea0d740861fab83567765e464bd9c
xyz <1192624880@qq.com>
2022-03-31 10:25:23 +0800

    提交文件

---
+++ app/src/main/java/com/wangshuo/wslive/wslivedemo/LiveActivity.java
@@ -0,0 +1,84 @@
+package com.wangshuo.wslive.wslivedemo;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+
+import me.lake.librestreaming.core.listener.RESConnectionListener;
+import me.lake.librestreaming.filter.hardvideofilter.BaseHardVideoFilter;
+import me.lake.librestreaming.filter.hardvideofilter.HardVideoGroupFilter;
+import me.lake.librestreaming.ws.StreamAVOption;
+import me.lake.librestreaming.ws.StreamLiveCameraView;
+import me.lake.librestreaming.ws.filter.hardfilter.GPUImageBeautyFilter;
+import me.lake.librestreaming.ws.filter.hardfilter.WatermarkFilter;
+import me.lake.librestreaming.ws.filter.hardfilter.extra.GPUImageCompatibleFilter;
+
+public class LiveActivity extends AppCompatActivity {
+    private static final String TAG = LiveActivity.class.getSimpleName();
+    private StreamLiveCameraView mLiveCameraView;
+    private StreamAVOption streamAVOption;
+    //private String rtmpUrl = "rtmp://ossrs.net/" + StatusBarUtils.getRandomAlphaString(3) + '/' + StatusBarUtils.getRandomAlphaDigitString(5);
+    private String rtmpUrl = "rtmp://jiankong.ujianchina.net:802/live/yuezhitest";
+
+    private LiveUI mLiveUI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+        StatusBarUtils.setTranslucentStatus(this);
+
+        initLiveConfig();
+        mLiveUI = new LiveUI(this,mLiveCameraView,rtmpUrl);
+    }
+
+    /**
+     * 设置推流参数
+     */
+    public void initLiveConfig() {
+        mLiveCameraView = (StreamLiveCameraView) findViewById(R.id.stream_previewView);
+
+        //参数配置 start
+        streamAVOption = new StreamAVOption();
+        streamAVOption.streamUrl = rtmpUrl;
+        //参数配置 end
+
+        mLiveCameraView.init(this, streamAVOption);
+        mLiveCameraView.addStreamStateListener(resConnectionListener);
+        LinkedList<BaseHardVideoFilter> files = new LinkedList<>();
+        files.add(new GPUImageCompatibleFilter(new GPUImageBeautyFilter()));    //滤镜
+        files.add(new WatermarkFilter(BitmapFactory.decodeResource(getResources(),R.mipmap.live),new Rect(100,100,200,200)));   //水印
+        mLiveCameraView.setHardVideoFilter(new HardVideoGroupFilter(files));
+    }
+
+    RESConnectionListener resConnectionListener = new RESConnectionListener() {
+        @Override
+        public void onOpenConnectionResult(int result) {
+            //result 0成功  1 失败
+            Toast.makeText(LiveActivity.this,"打开推流连接 状态："+result+ " 推流地址："+rtmpUrl,Toast.LENGTH_LONG).show();
+            Log.e(TAG, "推流地址：" + rtmpUrl);
+        }
+
+        @Override
+        public void onWriteError(int errno) {
+            Toast.makeText(LiveActivity.this,"推流出错,请尝试重连",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCloseConnectionResult(int result) {
+            //result 0成功  1 失败
+            Toast.makeText(LiveActivity.this,"关闭推流连接 状态："+result,Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLiveCameraView.destroy();
+    }
+}
---
+++ libWSLive/src/main/obj/local/armeabi-v7a/objs/resrtmp/F_/AndroidDemo/WSLiveDemo-master/libWSLive/src/main/jni/rtmp/librtmp/parseurl.o.d
@@ -0,0 +1,14 @@
+F:/AndroidDemo/WSLiveDemo-master/libWSLive/src/main/obj/local/armeabi-v7a/objs/resrtmp/F_/AndroidDemo/WSLiveDemo-master/libWSLive/src/main/jni/rtmp/librtmp/parseurl.o: \
+  F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\parseurl.c \
+  F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\rtmp_sys.h \
+  F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\rtmp.h \
+  F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\amf.h \
+  F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\log.h
+
+F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\rtmp_sys.h:
+
+F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\rtmp.h:
+
+F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\amf.h:
+
+F:\AndroidDemo\WSLiveDemo-master\libWSLive\src\main\jni\rtmp\librtmp\log.h:
